f2ba98827b2cbadd5be31521a4f56395e50323e7
syphan <phantiensy@unimarmores.com>
2019-12-13 11:48:15 +0700

    N + 1 problem in Hibernate example

--- com-syphan-registration/com-syphan-registration-dao/src/main/java/com/syphan/practice/registration/dao/UserRepository.java
+++ com-syphan-registration/com-syphan-registration-dao/src/main/java/com/syphan/practice/registration/dao/UserRepository.java
@@ -2,7 +2,10 @@ package com.syphan.practice.registration.dao;
 
 import com.syphan.practice.common.dao.JpaQueryRepository;
 import com.syphan.practice.registration.api.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaQueryRepository<User, Integer> {
@@ -11,4 +14,7 @@ public interface UserRepository extends JpaQueryRepository<User, Integer> {
     User findByEmail(String email);
 
     User findByPhoneNum(String phoneNum);
+
+    @Query("select u from User u join fetch u.roles")
+    List<User> findAllByLazy();
 }
